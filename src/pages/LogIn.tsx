46b0438588a97da53bca744541c757ef22872fe3
Jahidul Islam <jahid.redmi10@gmail.com>
2025-08-28 00:06:01 +0600

    create login from

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,3 +1,138 @@
-export default function LogIn() {
-  return <div className="max-w-7xl mx-auto">this is log in page</div>;
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import type { HTMLAttributes } from "react";
+import z from "zod/v3";
+
+import imag from "../../src/assets/athentication/login.svg";
+import { Link } from "react-router";
+import Password from "@/components/Password";
+type RegisterProps = HTMLAttributes<HTMLDivElement>;
+
+export default function LogIn({ className, ...props }: RegisterProps) {
+  const { reset } = useForm();
+  const logInSchema = z.object({
+    email: z.string().email({ message: "please provide a valid email" }),
+    password: z
+      .string()
+      .min(10, {
+        message:
+          "password must be at least 10 characters or special charecter.",
+      })
+      .max(20, {
+        message: "password exceed 20 charecter long",
+      }),
+  });
+
+  const form = useForm<z.infer<typeof logInSchema>>({
+    resolver: zodResolver(logInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof logInSchema>) => {
+    console.log(data);
+    reset();
+  };
+  return (
+    <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
+      <div className={cn("flex flex-col gap-6", className)} {...props}>
+        <div className="mx-auto items-center">
+          <h2 className="capitalize text-3xl lg:text-4xl md:text-5xl">
+            please login
+          </h2>
+        </div>
+        <Card className="overflow-hidden p-0">
+          <CardContent className="grid p-6 md:grid-cols-2 mx-auto items-center gap-4">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-5 md:px-10"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>email</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="enter your email"
+                          type="email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>password</FormLabel>
+                      <FormControl>
+                        <Password {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button className="w-full" type="submit">
+                  Submit
+                </Button>
+                <div>
+                  <span className="flex items-center">
+                    <span className="h-px flex-1 bg-gradient-to-r from-transparent to-gray-300 dark:to-gray-600"></span>
+
+                    <span className="shrink-0 px-4 text-gray-900 dark:text-white">
+                      Or
+                    </span>
+
+                    <span className="h-px flex-1 bg-gradient-to-l from-transparent to-gray-300 dark:to-gray-600"></span>
+                  </span>
+                </div>
+                <Button
+                  className="w-full bg-black hover:bg-blue-700"
+                  type="submit"
+                >
+                  google
+                </Button>
+                <div>
+                  <h2>
+                    Don’t have an account?{" "}
+                    <span className="font-bold">
+                      <Link to="/register">Register</Link>
+                    </span>
+                  </h2>
+                </div>
+              </form>
+            </Form>
+            <div className="hidden sm:block">
+              <img src={imag} alt="" />
+            </div>
+          </CardContent>
+        </Card>
+        <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+          By clicking continue, you agree to our{" "}
+          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
-
 import { useForm } from "react-hook-form";
 import {
   Form,
@@ -18,46 +16,70 @@ import type { HTMLAttributes } from "react";
 import z from "zod/v3";
 
 import imag from "../../src/assets/athentication/login.svg";
+import { Link } from "react-router";
+import Password from "@/components/Password";
 type RegisterProps = HTMLAttributes<HTMLDivElement>;
 
 export default function Register({ className, ...props }: RegisterProps) {
-  const formSchema = z.object({
-    name: z.string().min(2, {
-      message: "Username must be at least 2 characters.",
-    }),
-    email: z.string().email({ message: "please provide a valid email" }),
-    password: z
-      .string()
-      .min(10, {
-        message:
-          "password must be at least 10 characters or special charecter.",
-      })
-      .max(20, {
-        message: "password exceed 20 charecter long",
+  const { reset } = useForm();
+  const registerSchema = z
+    .object({
+      name: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
       }),
-  });
+      email: z.string().email({ message: "please provide a valid email" }),
+      password: z
+        .string()
+        .min(10, {
+          message:
+            "password must be at least 10 characters or special charecter.",
+        })
+        .max(20, {
+          message: "password exceed 20 charecter long",
+        }),
+      ConfrimPassword: z
+        .string()
+        .min(10, {
+          message:
+            "password must be at least 10 characters or special charecter.",
+        })
+        .max(20, {
+          message: "password exceed 20 charecter long",
+        }),
+    })
+    .refine((data) => data.ConfrimPassword === data.password, {
+      message: "password do  not match",
+      path: ["ConfrimPassword"],
+    });
 
-  const form = useForm({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
     defaultValues: {
       name: "",
       email: "",
       password: "",
+      ConfrimPassword: "",
     },
   });
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: z.infer<typeof registerSchema>) => {
     console.log(data);
+    reset();
   };
   return (
     <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
       <div className={cn("flex flex-col gap-6", className)} {...props}>
+        <div className="mx-auto items-center">
+          <h2 className="capitalize text-3xl lg:text-4xl md:text-5xl">
+            register
+          </h2>
+        </div>
         <Card className="overflow-hidden p-0">
           <CardContent className="grid p-6 md:grid-cols-2 mx-auto items-center gap-4">
             <Form {...form}>
               <form
                 onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8"
+                className="space-y-5 md:px-10"
               >
                 <FormField
                   control={form.control}
@@ -80,7 +102,11 @@ export default function Register({ className, ...props }: RegisterProps) {
                     <FormItem>
                       <FormLabel>email</FormLabel>
                       <FormControl>
-                        <Input placeholder="enter your email" {...field} />
+                        <Input
+                          placeholder="enter your email"
+                          type="email"
+                          {...field}
+                        />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -91,17 +117,56 @@ export default function Register({ className, ...props }: RegisterProps) {
                   name="password"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>email</FormLabel>
+                      <FormLabel>password</FormLabel>
                       <FormControl>
-                        <Input placeholder="enter your email" {...field} />
+                        <Password {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
+                <FormField
+                  control={form.control}
+                  name="ConfrimPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Confrim Password</FormLabel>
+                      <FormControl>
+                        <Password {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
                 <Button className="w-full" type="submit">
                   Submit
                 </Button>
+                <div>
+                  <span className="flex items-center">
+                    <span className="h-px flex-1 bg-gradient-to-r from-transparent to-gray-300 dark:to-gray-600"></span>
+
+                    <span className="shrink-0 px-4 text-gray-900 dark:text-white">
+                      Or
+                    </span>
+
+                    <span className="h-px flex-1 bg-gradient-to-l from-transparent to-gray-300 dark:to-gray-600"></span>
+                  </span>
+                </div>
+                <Button
+                  className="w-full bg-black hover:bg-blue-700"
+                  type="submit"
+                >
+                  google
+                </Button>
+                <div>
+                  <h2>
+                    already have an account?{" "}
+                    <span className="font-bold">
+                      <Link to="/login">login</Link>
+                    </span>
+                  </h2>
+                </div>
               </form>
             </Form>
             <div className="hidden sm:block">
